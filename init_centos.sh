b797c44ac8e368fe4b75eb3b73ed347b883cea0c
daimaqiao <daimaqiao@126.com>
2020-06-27 18:20:02 +0800

    first commit

---
+++ init_centos.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# for centos7
+sudo yum install -y git make gcc gcc-c++
+## sudo yum install -y unzip autoconf automake libtool
+
+sudo yum install -y speex-devel bzip2-devel zlib-devel
+
+sudo yum install -y epel-release
+
+## curl https://www.nasm.us/nasm.repo
+cat << EOF | sudo tee /etc/yum.repos.d/nasm.repo
+[nasm]
+name=The Netwide Assembler
+baseurl=http://www.nasm.us/pub/nasm/stable/linux/
+enabled=1
+gpgcheck=0
+
+[nasm-testing]
+name=The Netwide Assembler (release candidate builds)
+baseurl=http://www.nasm.us/pub/nasm/testing/linux/
+enabled=0
+gpgcheck=0
+
+[nasm-snapshot]
+name=The Netwide Assembler (daily snapshot builds)
+baseurl=http://www.nasm.us/pub/nasm/snapshots/latest/linux/
+enabled=0
+gpgcheck=0
+EOF
+
+sudo yum install -y nasm
